617a7b3021d06fbae71dd45e8c4c2474a3d057df
Cyprien Siaud <74303569+CSIAUD@users.noreply.github.com>
2022-10-27 06:23:48 +0200

    25/10/22

--- src/index.js
+++ src/index.js
@@ -1,41 +1,11 @@
-/* Objectif :
-    L’objectif de ce TP est de créer un service d’accès à des posts (contenu textuel) avec contrôle 
-    d’accès
-
-    Utilisateurs :
-        - ID : 777  PSEUDO : admin  ADMIN : true    MDP : admin
-        - ID : 123  PSEUDO : bob    ADMIN : false   MDP : coucou
-        - ID : 321  PSEUDO : alice  ADMIN : false   MDP : coucou
-
-    Spécifications fonctionnelles :
-
-        JWT :
-            Le service doit exposer un moyen de générer et obtenir un JWT.
-            Le JWT doit avoir une durée de validité de 1 jour;
-            (Seul un utilisateur authentifié peut demander la génération et l'obtention de son token)
-
-        Accès au ressources :
-            On doit pouvoir consulter des posts (ressources) :
-            - ID : 456  AUTEUR : 123 (bob)      CONTENU : Je suis un bob.
-            - ID : 654  AUTEUR : 321 (alice)    CONTENU : Je suis au pays des merveilles.
-            - ID : 555  AUTEUR : 777 (admin)    CONTENU : Je suis tout puissant.
-
-            Les utilisateurs non-admin n’ont accès qu’aux posts dont ils sont l’auteur.
-            Par exemple, bob n’a accès qu’au post 456 et alice qu’au post 654.
-            
-            Les utilisateurs admin ont accès à tous les posts.
-
-        Toutes les réponses doivent être en application/json
-*/
-
 
 // readFileSync lis les données de façon synchrone
 const fs = require('fs');
-let userFile = fs.readFileSync('users.json');
+let userFile = fs.readFileSync('./data/users.json');
 let users = JSON.parse(userFile);
 // console.log(users);
 
-let postFile = fs.readFileSync('posts.json');
+let postFile = fs.readFileSync('./data/posts.json');
 let posts = JSON.parse(postFile);
 // console.log(posts);
 
@@ -121,10 +91,7 @@ app.post('/refreshToken', (req, res) => {
     }
 
     jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) {
-        return res.sendStatus(401);
-        }
-        // TODO : check en bdd que le user a toujours les droit et qu'il existe toujours
+        if (err) return res.sendStatus(401);
         delete user.iat;
         delete user.exp;
         const refreshedToken = generateAccessToken(user);
